fc611570a5d57aaab07246e1873008828a1d98d8
MhdJavithF <javithmd001@gmail.com>
2025-05-18 01:36:31 +0530

    scroll, animation and redesign

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const backToTopButton = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+});
+
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+const toggle = document.getElementById('darkToggle');
+  toggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+});
+
 function toggleMobileMenu(){
 	document.getElementById("menu").classList.toggle("active");
 };
